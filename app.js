3150b7d80804d4c6a4cd8eac49f57f6794e10e37
Kenshi <james85529@gmail.com>
2023-06-08 00:15:41 +0800

    send get request wit https module

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-import express from 'express';
+import express from "express";
+import https from "https";
 
 const app = express();
-const port = 3000
+const port = 3000;
 
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?q=Taipei&appid=74837014de69dcf6842d8ae5780f4a95";
 
-app.get("/",(req,res)=>{
-    res.send('Server is working.')
-})
+app.get("/", (req, res) => {
+  https.get(url, (res) => {
+    console.log(res);
+  });
+  res.send("Server is working.");
+});
 
-app.listen(port,function(){
-    console.log("Server is listening on port %d", port)
-})
+app.listen(port, function () {
+  console.log("Server is listening on port %d", port);
+});
